3781b7af2bb3bb31668ac859c6422b1a824691b8
unknown <opect.pol@i.ua>
2019-06-18 16:18:01 +0300

    18/06/2019/first

---
+++ module-11/js/index.js
@@ -0,0 +1,136 @@
+"use strict";
+
+const laptops = [
+  {
+    size: 13,
+    color: 'white',
+    price: 28000,
+    release_date: 2015,
+    name: 'Macbook Air White 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'gray',
+    price: 32000,
+    release_date: 2016,
+    name: 'Macbook Air Gray 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 13,
+    color: 'black',
+    price: 35000,
+    release_date: 2017,
+    name: 'Macbook Air Black 13"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'white',
+    price: 45000,
+    release_date: 2015,
+    name: 'Macbook Air White 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'gray',
+    price: 55000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 15,
+    color: 'black',
+    price: 45000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 15"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'white',
+    price: 65000,
+    release_date: 2015,
+    name: 'Macbook Air White 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'gray',
+    price: 75000,
+    release_date: 2016,
+    name: 'Macbook Pro Gray 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+  {
+    size: 17,
+    color: 'black',
+    price: 80000,
+    release_date: 2017,
+    name: 'Macbook Pro Black 17"',
+    img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+    descr:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+  },
+];
+
+const formLaptop = document.querySelector('.js-form');
+const list = document.querySelector('.laptops-list');
+const source = document.querySelector('#laptops-card').innerHTML.trim();
+const template = Handlebars.compile(source);
+const resetBtn = document.querySelector('button[type=reset]');
+let markup = template(laptops);
+
+
+formLaptop.addEventListener('submit', filtredListLaptop);
+resetBtn.addEventListener('click', resetList);
+list.insertAdjacentHTML('afterbegin', markup);
+
+function filtredListLaptop(event) {
+  event.preventDefault();
+  const filter = getArrayCheckedInputs().reduce((acc, elem) => {
+    acc[elem.name]
+      ? acc[elem.name].push(elem.value)
+      : (acc[elem.name] = [elem.value]);
+    return acc;
+  }, {});
+  markup = template(filterCheck(filter, laptops));
+  list.innerHTML = markup;
+}
+
+function getArrayCheckedInputs() {
+  return Array.from(formLaptop.querySelectorAll('input[type="checkbox"]:checked')
+  );
+}
+
+function filterCheck(arr, laptops) {
+  const resultFilter = Object.keys(arr).reduce(
+    (a, e) => a.filter(elem => arr[e].includes("" + elem[e])),
+    laptops
+  );
+  return resultFilter;
+}
+
+function resetList() {
+  let markup = template(laptops);
+  return list.innerHTML = markup;
+}
