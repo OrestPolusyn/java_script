3b8b93497d10078e32ee3a440a702750d90c8ae3
unknown <opect.pol@i.ua>
2019-06-23 16:52:46 +0300

    dont update

--- module-13/src/services/storage.js
+++
@@ -1,8 +0,0 @@
-export const set = value => {
-    localStorage.setItem("bookmark", JSON.stringify(value));
-};
-
-export const get = () => {
-    const data = localStorage.getItem("bookmark");
-    return data ? JSON.parse(data) : [];
-};
